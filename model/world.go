f329bba80d08c5156ce63bd6776aa833d4993e7a
Mike Sergeant <sergeant.mj@gmail.com>
2017-12-10 22:26:08 -0500

    Implement GenerateNextIteration functionality

--- model/world.go
+++ model/world.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -8,10 +9,12 @@ type World struct {
 	liveCells [][]int64
 }
 
+// NewWorld returns initialized World
 func NewWorld(cells [][]int64) World {
 	return World{cells}
 }
 
+// IsAlive returns true if the given cell is currently alive
 func (w World) IsAlive(cell []int64) bool {
 	for _, liveCell := range w.liveCells {
 		if reflect.DeepEqual(cell, liveCell) {
@@ -21,5 +24,50 @@ func (w World) IsAlive(cell []int64) bool {
 	return false
 }
 
+// GenerateNextIteration returns the next generation of the World
 func (w World) GenerateNextIteration() World {
+	liveCells := [][]int64{}
+	for _, cell := range w.cellsToTest() {
+		liveNeighbors := w.liveNeighborCount(cell)
+
+		if (w.IsAlive(cell) && liveNeighbors == 2) || liveNeighbors == 3 {
+			liveCells = append(liveCells, cell)
+		}
+	}
+	return NewWorld(liveCells)
+}
+
+func (w World) cellsToTest() (output [][]int64) {
+	setToTest := make(map[string][]int64)
+
+	for _, liveCell := range w.liveCells {
+		neighbors := neighborCells(liveCell)
+		for _, cell := range neighbors {
+			setToTest[fmt.Sprintf("%d,%d", cell[0], cell[1])] = cell
+		}
+	}
+
+	for _, value := range setToTest {
+		output = append(output, value)
+	}
+
+	return
+}
+
+func (w World) liveNeighborCount(cell []int64) (output uint8) {
+	for _, neighbor := range neighborCells(cell) {
+		if w.IsAlive(neighbor) {
+			output += 1
+		}
+	}
+
+	return
+}
+
+func neighborCells(cell []int64) [][]int64 {
+	x, y := cell[0], cell[1]
+	return [][]int64{
+		{x - 1, y - 1}, {x - 0, y - 1}, {x + 1, y - 1},
+		{x - 1, y - 0}, {x + 1, y - 0},
+		{x - 1, y + 1}, {x - 0, y + 1}, {x + 1, y + 1}}
 }
--- model/world_test.go
+++ model/world_test.go
@@ -48,3 +48,19 @@ func TestIsAliveReturnsTrue(t *testing.T) {
 		}
 	}
 }
+
+func TestGenerateNextIteration(t *testing.T) {
+	points := [][]int64{{7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6}, {7, 7}, {7, 8}, {7, 9}}
+	expectedPoints := [][]int64{
+		{6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5}, {6, 6}, {6, 7}, {6, 8},
+		{7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6}, {7, 7}, {7, 8},
+		{8, 1}, {8, 2}, {8, 3}, {8, 4}, {8, 5}, {8, 6}, {8, 7}, {6, 8}}
+
+	world := NewWorld(points).GenerateNextIteration()
+
+	for _, cell := range expectedPoints {
+		if !world.IsAlive(cell) {
+			t.Errorf("Expected IsAlive to return true, got false for %d", cell)
+		}
+	}
+}
