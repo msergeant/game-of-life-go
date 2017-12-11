f87ac4271587712150e24bb684e9ea91ae980df7
Mike Sergeant <sergeant.mj@gmail.com>
2017-12-10 21:30:34 -0500

    Add IsAlive functionality

--- model/world.go
+++ model/world.go
@@ -1,13 +1,25 @@
 package model
 
-type Point struct {
-	x, y int64
-}
+import (
+	"reflect"
+)
 
 type World struct {
-	liveCells []Point
+	liveCells [][]int64
 }
 
 func NewWorld(cells [][]int64) World {
-	return World{}
+	return World{cells}
+}
+
+func (w World) IsAlive(cell []int64) bool {
+	for _, liveCell := range w.liveCells {
+		if reflect.DeepEqual(cell, liveCell) {
+			return true
+		}
+	}
+	return false
+}
+
+func (w World) GenerateNextIteration() World {
 }
--- model/world_test.go
+++ model/world_test.go
@@ -10,3 +10,41 @@ func TestNewWorldEmpty(t *testing.T) {
 		t.Errorf("Initializing an empty world failed")
 	}
 }
+
+func TestNewWorldWithData(t *testing.T) {
+	points := [][]int64{
+		{0, 0},
+		{1, 1},
+		{3, 3}}
+	world := NewWorld(points)
+
+	if len(world.liveCells) != len(points) {
+		t.Errorf("World had incorrect number of initial points, expected %d, got %d", len(points), len(world.liveCells))
+	}
+}
+
+func TestIsAliveReturnsFalse(t *testing.T) {
+	points := [][]int64{
+		{0, 0},
+		{1, 1},
+		{3, 3}}
+	world := NewWorld(points)
+
+	if world.IsAlive([]int64{2, 2}) {
+		t.Errorf("Expected IsAlive to return false, got true")
+	}
+}
+
+func TestIsAliveReturnsTrue(t *testing.T) {
+	points := [][]int64{
+		{0, 0},
+		{1, 1},
+		{3, 3}}
+	world := NewWorld(points)
+
+	for _, cell := range points {
+		if !world.IsAlive(cell) {
+			t.Errorf("Expected IsAlive to return true, got false for %d", cell)
+		}
+	}
+}
